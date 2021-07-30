b08d2d63217dd06573037f809877355d094f1369
gabrielnsd <gabrielnsdantas@gmail.com>
2021-07-30 14:46:39 -0300

    rank in aws lambda

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Rank = ({name, entries}) => {
+const Rank = ({ name, entries }) => {
+  const [emoji, setEmoji] = useState("");
+  useEffect(() => {
+    generateEmoji(entries);
+  }, [entries]);
+
+  const generateEmoji = async (entries) => {
+    try {
+      const response = await fetch(
+        `https://h4jfxb28qi.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`
+      );
+      const data = await response.json();
+      setEmoji(data.input);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div>
-      <div className="white f3">{`${name}, your current entry count is...` }</div>
+      <div className="white f3">{`${name}, your current entry count is...`}</div>
       <div className="white f1">{entries}</div>
+      <div className="white f3">Rank badge: {emoji}</div>
     </div>
   );
 };
